02f9535e77ce052cc02d2f6ee549fccbf5b82ae0
Cesar Koprowski <195948232+cesarkoprowski-rm@users.noreply.github.com>
2025-03-25 15:10:03 -0300

    Add description container

--- pokemon-tcg/src/App.vue
+++ pokemon-tcg/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <section>
     <Navbar/>
+    <Decription/>
   </section>
 </template>
 
 <script setup>
   import Navbar from './components/Navbar.vue';
+  import Decription from './components/Decription.vue';
 </script>
  
---
+++ pokemon-tcg/src/components/Decription.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="bg-black py-20 mb-4">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+      <div class="text-center">
+        <h1 class="text-4xl font-extrabold text-yellow-500 sm:text-5xl md:text-6xl">
+          Become a real Pokémon trainer
+        </h1>
+        <p class="my-4 text-xl text-white">
+          Find the Pokémon Card that you want!
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+ 
+<script setup>
+ 
+</script>
